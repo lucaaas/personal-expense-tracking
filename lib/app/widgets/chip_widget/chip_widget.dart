b618646de99fed00bda0d8e660427f42dac64479
Lucas <lucasoliveira2306@gmail.com>
2025-02-21 15:38:51 -0300

    chip_widget (feat): create widget #9

--- lib/app/widgets/chip_widget/chip_widget.dart
+++ lib/app/widgets/chip_widget/chip_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 class ChipWidget extends StatefulWidget {
   final Color color;
   final String label;
+  final IconData? icon;
   final bool isSelected;
   final void Function(bool) onChange;
 
@@ -11,6 +12,7 @@ class ChipWidget extends StatefulWidget {
     required this.color,
     required this.label,
     required this.onChange,
+    this.icon,
     this.isSelected = false,
   });
 
@@ -34,19 +36,50 @@ class _ChipWidgetState extends State<ChipWidget> {
     });
   }
 
+  Color _getContrastingTextColor(Color backgroundColor) {
+    if (_isSelected) {
+      double brightness =
+          (backgroundColor.red * 0.299 + backgroundColor.green * 0.587 + backgroundColor.blue * 0.114) / 255;
+
+      return brightness > 0.5 ? CupertinoColors.black : CupertinoColors.white;
+    } else {
+      return backgroundColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(4),
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-      decoration: BoxDecoration(
-        border: Border.all(color: widget.color, width: 2),
-        color: _isSelected ? widget.color : CupertinoColors.transparent,
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: GestureDetector(
-        onTap: _onTap,
-        child: Text(widget.label, style: const TextStyle(fontSize: 16)),
+    return IntrinsicWidth(
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        padding: EdgeInsets.symmetric(horizontal: widget.icon != null ? 5 : 10, vertical: 2),
+        decoration: BoxDecoration(
+          border: Border.all(color: widget.color, width: 2),
+          color: _isSelected ? widget.color : CupertinoColors.transparent,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: GestureDetector(
+          onTap: _onTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              widget.icon != null
+                  ? Icon(
+                      widget.icon,
+                      color: _getContrastingTextColor(widget.color),
+                      size: 16,
+                    )
+                  : Container(),
+              Text(
+                widget.label,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: _getContrastingTextColor(widget.color),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
