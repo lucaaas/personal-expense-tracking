8227976d8083adf8f5111a8ac2262152a0940aca
Lucas <lucasoliveira2306@gmail.com>
2025-02-23 19:09:25 -0300

    get_constrasting_text_color (fix): consider opacity before calc #9

--- lib/app/helpers/get_constrasting_text_color.dart
+++ lib/app/helpers/get_constrasting_text_color.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
 Color getContrastingTextColor(Color backgroundColor) {
-  double brightness =
-      (backgroundColor.red * 0.299 + backgroundColor.green * 0.587 + backgroundColor.blue * 0.114) / 255;
+  if (backgroundColor.opacity < 0.5) {
+    return CupertinoColors.black;
+  }
 
-  return brightness > 0.5 ? CupertinoColors.black : CupertinoColors.white;
+  double brightness =
+      ((backgroundColor.red * 0.299 + backgroundColor.green * 0.587 + backgroundColor.blue * 0.114) / 255) *
+          backgroundColor.opacity;
+
+  return brightness * backgroundColor.opacity > 0.5 ? CupertinoColors.black : CupertinoColors.white;
 }
