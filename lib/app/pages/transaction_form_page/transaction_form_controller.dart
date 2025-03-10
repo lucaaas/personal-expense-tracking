b5bbfee794b79f78ce350d6c4bfcfa3644ac8b3e
Lucas <lucasoliveira2306@gmail.com>
2025-03-10 15:35:03 -0300

    transaction_form (feat): create form #9

---
+++ lib/app/pages/transaction_form_page/transaction_form_controller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:personal_expense_tracker/app/models/category_model.dart';
+import 'package:personal_expense_tracker/app/models/credit_card_model.dart';
+import 'package:personal_expense_tracker/app/models/transaction_model.dart';
+
+enum TransactionType { expense, income }
+
+class TransactionFormController {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TransactionModel transaction;
+
+  TransactionType type = TransactionType.expense;
+
+  TransactionFormController({TransactionModel? transaction}) : transaction = transaction ?? TransactionModel.empty() {}
+
+  Future<void> save() async {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+
+      if (type == "expense") {
+        transaction.value = -transaction.value;
+      }
+
+      await transaction.save();
+      print(transaction.id);
+    } else {
+      throw Exception("Invalid form");
+    }
+  }
+
+  Future<List<CategoryModel>> getCategories() {
+    return CategoryModel.list();
+  }
+
+  Future<List<CreditCardModel>> getCredicards() async {
+    return CreditCardModel.list();
+  }
+}
