18832d7abe8daf00572ea47daf509c2ca859e2c6
Lucas <lucasoliveira2306@gmail.com>
2025-03-25 12:15:58 -0300

    tabbar_widget (feat): add scroll to show the selected tab #7

--- lib/app/widgets/tabbar_widget/tabbar_widget.dart
+++ lib/app/widgets/tabbar_widget/tabbar_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 
-class TabBarWidget extends StatelessWidget {
+class TabBarWidget extends StatefulWidget {
   final List<TabBarItem> items;
-  final int? selectedIndex;
+  final int selectedIndex;
   final void Function(int)? onTabChanged;
 
   const TabBarWidget({
@@ -11,6 +11,27 @@ class TabBarWidget extends StatelessWidget {
     this.selectedIndex = 0,
     this.onTabChanged,
   });
+
+  @override
+  State<TabBarWidget> createState() => _TabBarWidgetState();
+}
+
+class _TabBarWidgetState extends State<TabBarWidget> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void didUpdateWidget(covariant TabBarWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _scrollController.animateTo(_scrollOffset, duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(_scrollOffset, duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,16 +48,17 @@ class TabBarWidget extends StatelessWidget {
       ),
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
+        controller: _scrollController,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: items
+          children: widget.items
               .map<Widget>(
                 (item) => GestureDetector(
-                  onTap: () => onTabChanged?.call(items.indexOf(item)),
+                  onTap: () => widget.onTabChanged?.call(widget.items.indexOf(item)),
                   child: _TabBarItemWidget(
                     item: item,
-                    isSelected: items.indexOf(item) == selectedIndex,
-                    itemWidth: items.length == 1 ? MediaQuery.of(context).size.width : null,
+                    isSelected: widget.items.indexOf(item) == widget.selectedIndex,
+                    itemWidth: _itemWidth,
                   ),
                 ),
               )
@@ -44,6 +66,26 @@ class TabBarWidget extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  double get _scrollOffset {
+    double offset = 0;
+
+    if (widget.selectedIndex == widget.items.length - 1) {
+      offset = _scrollController.position.maxScrollExtent;
+    } else if (widget.selectedIndex > 1) {
+      offset = _itemWidth * (widget.selectedIndex - 1);
+    }
+
+    return offset;
+  }
+
+  double get _itemWidth {
+    if (widget.items.length > 3) {
+      return MediaQuery.of(context).size.width / 3;
+    } else {
+      return MediaQuery.of(context).size.width / widget.items.length;
+    }
   }
 }
 
