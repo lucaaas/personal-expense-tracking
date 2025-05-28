faa1cc37fe590f05ad1a032fb3ba2a52421486c4
Lucas <lucasoliveira2306@gmail.com>
2025-05-28 09:49:22 -0300

    card_widget (feat): add onTap callback to CardWidget #23

--- lib/app/widgets/card_widget/card_widget.dart
+++ lib/app/widgets/card_widget/card_widget.dart
@@ -5,8 +5,10 @@ class CardWidget extends StatelessWidget {
   final Widget? subtitle;
   final Icon? icon;
   final Widget? trailing;
+  final void Function()? onTap;
 
   final Widget? child;
+
   const CardWidget({
     super.key,
     this.child,
@@ -14,45 +16,49 @@ class CardWidget extends StatelessWidget {
     this.subtitle,
     this.icon,
     this.trailing,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-      margin: const EdgeInsets.symmetric(vertical: 5),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: CupertinoColors.inactiveGray,
-          width: 1,
-          style: BorderStyle.solid,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: CupertinoColors.inactiveGray,
+            width: 1,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          color: CupertinoColors.white,
+          boxShadow: [
+            BoxShadow(
+              color: CupertinoColors.black.withOpacity(0.1),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+          ],
         ),
-        borderRadius: const BorderRadius.all(Radius.circular(15)),
-        color: CupertinoColors.white,
-        boxShadow: [
-          BoxShadow(
-            color: CupertinoColors.black.withOpacity(0.1),
-            spreadRadius: 2,
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          CupertinoListTile(
-            padding: const EdgeInsets.fromLTRB(10, 5, 10, 15),
-            title: title,
-            leading: icon,
-            subtitle: subtitle,
-            trailing: trailing,
-          ),
-          if (child != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-              child: child,
-            )
-        ],
+        child: Column(
+          children: [
+            CupertinoListTile(
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 15),
+              title: title,
+              leading: icon,
+              subtitle: subtitle,
+              trailing: trailing,
+            ),
+            if (child != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                child: child,
+              )
+          ],
+        ),
       ),
     );
   }
