d0c68043d9b0d87beb8a2113176aa6d0e7cac916
Lucas <lucasoliveira2306@gmail.com>
2025-06-03 16:28:23 -0300

    app_routes (refactor): implement onGenerateRoute for named routes #10

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
-import 'package:personal_expense_tracker/app/pages/home_page/home_page.dart';
-import 'package:personal_expense_tracker/app/pages/loading_page/loading_page.dart';
-import 'package:personal_expense_tracker/app/pages/statement_page/statement_page.dart';
-import 'package:personal_expense_tracker/app/pages/tab_page/tab_page.dart';
 import 'package:personal_expense_tracker/app/providers/transaction_provider.dart';
 import 'package:personal_expense_tracker/app/utils/app_routes.dart';
 import 'package:provider/provider.dart';
-
-import 'app/pages/transaction_form_page/transaction_form_page.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
@@ -29,10 +23,10 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => TransactionProvider()),
       ],
-      child: CupertinoApp(
+      child: const CupertinoApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
-        theme: const CupertinoThemeData(
+        theme: CupertinoThemeData(
           primaryColor: Color.fromARGB(255, 249, 139, 95),
           primaryContrastingColor: Color.fromARGB(255, 113, 54, 29),
           barBackgroundColor: Color.fromARGB(255, 249, 139, 95),
@@ -53,13 +47,7 @@ class MyApp extends StatelessWidget {
             primaryColor: Color.fromARGB(255, 113, 54, 29),
           ),
         ),
-        routes: {
-          AppRoutes.HOME: (context) => const HomePage(),
-          AppRoutes.TRANSACTION_FORM: (context) => const TransactionFormPage(),
-          AppRoutes.STATEMENT: (context) => const StatementPage(),
-          AppRoutes.LOADING: (context) => const LoadingPage(),
-          AppRoutes.TAB: (context) => const TabPage(),
-        },
+        onGenerateRoute: AppRoutes.onGenerateRoute,
       ),
     );
   }
