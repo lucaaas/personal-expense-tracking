d6588df69139b271e499c97f1f81ac58ad9f0977
Lucas <lucasoliveira2306@gmail.com>
2025-10-01 10:53:49 -0300

    firebase_helper (refactor): transform into an abstract mixin class

--- lib/app/helpers/firebase_helper.dart
+++ lib/app/helpers/firebase_helper.dart
@@ -1,19 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FirebaseHelper {
-  static FirebaseHelper? _instance;
-  late FirebaseFirestore _firebase;
-
-  FirebaseHelper._() {
-    _firebase = FirebaseFirestore.instance;
-  }
-
-  static FirebaseHelper getInstance() {
-    _instance ??= FirebaseHelper._();
-    return _instance!;
-  }
-
-  Future<void> addData(String collection, Map<String, dynamic> data, String id) async {
+abstract mixin class FirebaseHelper {
+  final FirebaseFirestore _firebase = FirebaseFirestore.instance;
+  
+  Future<void> addData(String id, Map<String, dynamic> data) async {
     await _firebase.collection(collection).doc(id).set(data);
   }
+
+  String get collection;
 }
