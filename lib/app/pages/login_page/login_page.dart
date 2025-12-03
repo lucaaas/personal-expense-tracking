450b59fabd5c316be057a457c9971b5c99ed4be4
Lucas <lucasoliveira2306@gmail.com>
2025-12-03 09:46:02 -0300

    login_page (refact): add Google Sign-In button with logo

--- lib/app/pages/login_page/login_page.dart
+++ lib/app/pages/login_page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:personal_expense_tracker/app/helpers/snack_bar_helper.dart';
 import 'package:personal_expense_tracker/app/pages/login_page/login_page_controller.dart';
 import 'package:personal_expense_tracker/app/utils/app_routes.dart';
@@ -31,19 +32,29 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return PageScaffoldWidget(
-      child: Center(
-        child: Column(
-          children: [
-            CupertinoButton.filled(
-              onPressed: _signInWithGoogle,
-              child: const Text('Entrar com Google'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          CupertinoButton(
+            onPressed: _signInWithGoogle,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                border: BoxBorder.all(color: CupertinoTheme.of(context).primaryColor, width: 2),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Row(
+                spacing: 10,
+                children: [
+                  SvgPicture.asset('assets/logos/google.svg', semanticsLabel: 'google'),
+                  const Text('Entrar com Google'),
+                ],
+              ),
             ),
-            CupertinoButton(
-              onPressed: _goToNextPage,
-              child: const Text('Continuar como convidado'),
-            ),
-          ],
-        ),
+          ),
+          CupertinoButton(onPressed: _goToNextPage, child: const Text('Continuar como convidado')),
+        ],
       ),
     );
   }
